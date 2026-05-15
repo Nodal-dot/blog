73fdd7d9aef4f443a8f5160ce556d8d3f49a925e
Nodal-dot <zeroxhero@yandex.ru>
2026-05-15 21:37:44 +0300

    chore: remove search engine verification tags

--- src/app/[locale]/metadata.ts
+++ src/app/[locale]/metadata.ts
@@ -76,10 +76,6 @@ export async function createPageMetadata({
                 },
             ],
         },
-        verification: {
-            google: "E1fyhFt-L0uYLatxqh9wi2RRKwvjcEZRHZh2Nwpzs7c",
-            yandex: "7450db46c3f77c36",
-        },
         other: {
             "msapplication-config": "/browserconfig.xml",
             "msapplication-TileColor": "#ffffff",
